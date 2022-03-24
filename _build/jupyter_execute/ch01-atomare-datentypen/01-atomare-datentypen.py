6cc26d3406dd4e73c79fb51f1ae2c7950da8d1f9
martindroege <martin.droege@hu-berlin.de>
2022-03-24 18:07:59 +0100

    build book v0.7

--- _build/jupyter_execute/ch01-atomare-datentypen/01-atomare-datentypen.py
+++ _build/jupyter_execute/ch01-atomare-datentypen/01-atomare-datentypen.py
@@ -3,31 +3,6 @@
 
 # # Arbeiten mit atomaren Datentypen
 
-# ## Ressourcen 
-# 
-# Die [umfangreiche Dokumentation](https://docs.python.org/3/) zu Python bietet hilfreiche Tutorials, ein Glossar und vor allem die Spezifikationen der einzelnen Bibliotheken beziehungsweise *libraries*.[^fn1] Sie werden immer wieder auf sie zurückgreifen. 
-#  
-# Eine weitere wichtige Ressource, die Sie immer wieder aufsuchen werden, gleich ob Sie gerade anfangen, zu programmieren oder schon länger dabei sind, ist: [Stackoverflow](https://stackoverflow.com/). Sie werden gerade in den ersten Wochen und Monaten auf kaum ein Problem stoßen, auf das nicht zu irgendeinem Zeitpunkt in der Vergangenheit jemand vor Ihnen gestoßen ist. Lernen Sie voneinander! Wir Historiker:innen sitzen zwar gerne alleine am Schreibtisch, aber wenn es ums Coden geht, dann dürfen, ja *sollten* wir miteinander arbeiten. **Aber wichtig:** Auch beim Schreiben von Programmcode sollte man Zitierregeln beachten und angeben, wenn man einige Zeilen Code von jemand anderem übernommen hat.
-
-# ## Tutorials
-# 
-# Das *World Wide Web* ist voll mit vielen nützlichen Tutorials. Auch auf YouTube finden Sie viele empfehlenswerte Kanäle mit gut nachvollziehbaren Erklärungen, die Ihnen den Einstieg erleichtern und auch spezielle Programmbibliotheken praxisorientiert erläutern. Der Großteil der verfügbaren Angebote ist aber nicht unbedingt auf geisteswissenschaftliche Bedürfnisse zugeschnitten. Wir haben hier daher einige Empfehlungen für Sie zusammengetragen, die Ihnen im weiteren Verlauf nützlich sein können:
-# 
-# - Ein freies Lehrbuch ist zum Beispiel ["A Byte of Python"](https://python.swaroopch.com/index.html). Hier werden keine Vorkenntnisse benötigt. Es stellt sicherlich auch eine gute Ergänzung für diesen Kurs dar.
-# 
-# - Sehr gut geeignet für Einsteiger:innen mit geisteswissenschaftlichen Forschungsinteressen ist ["Automate the Boring Stuff with Python"](https://automatetheboringstuff.com/) von Al Sweigart.
-# 
-# - Folgert Karsdorp hat eine [interaktive Einführung](https://www.karsdorp.io/python-course/) speziell für Geisteswissenschaftler:innen erstellt, die ebenfalls sehr einsteigerfreundlich ist. Hier finden Sie weitere Jupyter-Notebooks, die Sie begleitend zu dieser Übung bearbeiten können, wenn Sie noch mehr coden wollen. Zudem finden Sie einige Themenblöcke, die wir im Rahmen dieser Übung nicht anschneiden können.
-# 
-# - Paul Vierthaler hat auf seinem YouTube-Kanal Tutorials für seinen [Kurs zur Textanalyse](https://www.youtube.com/playlist?list=PL6kqrM2i6BPIpEF5yHPNkYhjHm-FYWh17) zur Verfügung gestellt.
-# 
-# - Auch auf [The Programming Historian](https://programminghistorian.org/en/lessons/?topic=python) finden Sie zahlreiche Python-Lektionen für spezifische geisteswissenschaftliche Anwendungsszenarien.
-# 
-# - [Corey Schafer](https://www.youtube.com/user/schafer5/videos) bietet auf seinem YouTube-Kanal etliche Tutorials rundum Pythongrundlagen, Softwareentwicklung, aber auch Data Science. Dieser Kanal könnte für Sie vor allem im Anschluss an diesen Kurs interessant sein.
-# 
-# - Auf der Seite [PythonHumanities.com](https://pythonhumanities.com/) stellt William Mattingly ebenfalls zahlreiche Videos mit Bezug Python und Digital Humanities frei zur Verfügung.
-# 
-
 # ## Lernziele
 # 
 # Was Sie am Ende dieser Lektion beantworten können sollen:
@@ -58,7 +33,7 @@
 #  - [Hacking the Humanities](https://www.youtube.com/playlist?list=PL6kqrM2i6BPIpEF5yHPNkYhjHm-FYWh17) Episoden 1-5
 
 # :::{note}
-# Führen Sie Beispiel-Codeblöcke bitte stets aus. Wenn Sie das Notebook über Binder nutzen, führen Sie bitte die Beispiel-Codeblöcke stets aus. Sie können jederzeit weitere Text- oder Codeblöcke einfügen, um Informationen zu ergänzen oder frei zu coden.
+# Führen Sie die Beispiel-Codeblöcke bitte stets aus. Über Binder können Sie jederzeit weitere Text- oder Codeblöcke einfügen, um Informationen zu ergänzen oder frei zu coden.
 # :::
 
 # ## Let´s code
---
+++ home.md
@@ -0,0 +1,28 @@
+# Home
+
+````{div} full-width
+```{figure} img/unter-den-linden-hiltmann-logo.jpg
+:class: full-width
+:alt: Humboldt-Universität zu Berlin Hauptgebäude mit Logo
+:width: 1600px
+:align: center
+```
+````
+
+Das Jupyter Book richtet sich an Historiker:innen, die einen ersten anwendungsorientierten und interaktiven Einstieg in die Programmiersprache Python suchen. Neben den Grundlagen der Skriptsprache werden auch weitere Einsatzgebiete für Historiker:innen vorgestellt.
+
+Autor:innen: [Melanie Althage](https://www.geschichte.hu-berlin.de/de/bereiche-und-lehrstuehle/digital-history/personen/althage), [Martin Dröge](https://www.geschichte.hu-berlin.de/de/bereiche-und-lehrstuehle/digital-history/personen/dr-martin-droege), [Torsten Hiltmann](https://www.geschichte.hu-berlin.de/de/bereiche-und-lehrstuehle/digital-history/personen/torsten-hiltmann), [Philipp Schneider](https://www.geschichte.hu-berlin.de/de/bereiche-und-lehrstuehle/digital-history/personen/philipp-schneider-m-a)
+
+Homepage: [Professur für Digital History an der Humboldt-Universität zu Berlin](https://www.geschichte.hu-berlin.de/de/bereiche-und-lehrstuehle/digital-history)
+
+Blog: [Digital History Berlin. Digitale Geschichtswissenschaften an der Humboldt-Universität zu Berlin](https://dhistory.hypotheses.org/)
+
+
+Version 1.0 vom 24.04.2022
+
+Lizenz unter: 
+```{figure} img/cc-zero.png
+:alt: Lizenz CC Zero
+:width: 100px
+:align: left
+```
